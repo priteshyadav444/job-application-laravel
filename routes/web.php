a97dfc6f0b42015b7d744dac2c8f7d9b126bb197
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-10-25 00:56:10 +0530

    edit html added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,8 +25,29 @@ Route::get('/lists/create', [ListingController::class, 'create']);
 //store form data
 Route::post('/lists', [ListingController::class, 'store']);
 
+//Show Edit
+Route::get('/lists/{list}/edit', [ListingController::class, 'edit']);
 
 //Single List
-
 Route::get('/lists/{list}', [ListingController::class, 'show']);
 
+//update 
+Route::put('/lists/{list}', [ListingController::class, 'update']);
+
+// delete 
+Route::delete('/lists/{list}', [ListingController::class, 'destroy']);
+
+
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout']);
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login');
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
