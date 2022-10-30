969c00d14d6597c734c366f7b3e7949a9772ec65
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-10-30 13:52:53 +0530

    new theme colour added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,7 +19,7 @@
                 theme: {
                     extend: {
                         colors: {
-                            laravel: "#ef3b2d",
+                            laravel: "#00bfff",
                         },
                     },
                 },
